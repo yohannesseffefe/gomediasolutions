673abd29fb7dc2f12ee6d90e9ee94ca2bc2a3972
Yohannes Seffefe <yohannesseffefe@gmail.com>
2025-03-16 17:31:59 +0300

    update

--- app/components/Works/Works.tsx
+++ app/components/Works/Works.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Works() {
   return (
-    <div className="w-full flex align-middle justify-center bg-black gap-8">
-      <img src="/game1.png" className="w-[300px]" />
-      <img src="/game2.png" className="w-[300px]" />
-      <img src="/game1.png" className="w-[300px]" />
-      <img src="/game2.png" className="w-[300px]" />
+    <div className="w-full flex align-middle md:flex-row flex-col justify-center bg-black gap-8">
+      <img src="/game1.PNG" className="w-[300px]" />
+      <img src="/game2.PNG" className="w-[300px]" />
+      <img src="/game1.PNG" className="w-[300px] md:flex hidden" />
+      <img src="/game2.PNG" className="w-[300px] md:flex hidden" />
     </div>
   );
 }

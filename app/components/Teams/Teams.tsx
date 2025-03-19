7ae531a0228e8ba0e5794c82391a41606a6a0087
Yohannes Seffefe <yohannesseffefe@gmail.com>
2025-03-19 18:11:20 +0300

    update

--- app/components/Teams/Teams.tsx
+++ app/components/Teams/Teams.tsx
@@ -7,11 +7,45 @@ function Teams() {
         <h1 className="md:text-[45px] text-[22px] font-normal text-white">
           Teams
         </h1>
-        <div className="flex gap-3 justify-between items-center w-full md:flex-row flex-col">
+        <div className="flex gap-5 justify-between items-center w-full md:flex-row flex-col">
           <Team
             name={"Selam"}
             photo={"/selam.jpg"}
             position={"CEO - Founder"}
+          />
+          <Team
+            name={"Natnael Ghirma"}
+            photo={"/selam.jpg"}
+            position={"CEO - Founder"}
+          />
+          <Team
+            name={"Bisrategebriel Tewedros"}
+            photo={"/bisrat.jpg"}
+            position={"Project Manager"}
+          />
+          <Team
+            name={"Yohannes Seffefe"}
+            photo={"/yohannes.jpg"}
+            position={"Developer"}
+          />
+        </div>
+
+        <div className="flex gap-5 justify-between items-center w-full md:flex-row flex-col">
+          <Team
+            name={"Betelehem Lemma"}
+            photo={"/bethelehem.jpg"}
+            position={"Developer"}
+          />
+          <Team name={"Eyobe "} photo={"/eyob.jpg"} position={"Developer"} />
+          <Team
+            name={"Munir Burhan"}
+            photo={"/munir.jpg"}
+            position={"Advisor"}
+          />
+          <Team
+            name={"Eden Berihun"}
+            photo={"/eden.jpg"}
+            position={"Advisor"}
           />
         </div>
       </div>
--- app/components/Teams/components/Team/Team.tsx
+++ app/components/Teams/components/Team/Team.tsx
@@ -7,7 +7,9 @@ function Team({ name, photo, position }: any) {
         className="w-full h-[254px] object-cover  object-top rounded-[20px] overflow-hidden"
         src={photo}
       />
-      <h1 className="text-black md:text-[25px] text-[18px]">{name}</h1>
+      <h1 className="text-black md:text-[16px] text-[13px] mt-[20px]">
+        {name}
+      </h1>
       <p className="text-black md:text-[14px] text-[12px]">{position}</p>
     </div>
   );
